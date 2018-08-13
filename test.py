da214fe5d184092cd56b7e4e67e34c0022511268
despiegk <kristof@incubaid.com>
2018-08-13 07:55:23 +0200

    revert luke changes

---
+++ test.py
@@ -0,0 +1,22 @@
+
+from jumpscale import j
+
+
+
+community = j.servers.digitalme.community
+
+community.knowledge_learn("recipes")
+
+#now knowledge has been loaded
+
+# for i in range(100):
+#     assert (0, 11) == r2.action_ask("task1", 10)  # returns resultcode & result
+
+# assert True == r2.monitor_running()
+# assert True == r2.running()
+
+# community.start()
+
+
+j.servers.digitalme.community
+
